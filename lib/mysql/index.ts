124296169f3624fbfc144d8231890b198194b48b
tchen <tepelchen501@gmail.com>
2022-12-28 03:16:55 +0900

    ec2やめる

--- lib/mysql/index.ts
+++ lib/mysql/index.ts
@@ -4,8 +4,8 @@ export async function getMysql() {
   return await mysql2.createConnection({
     host     : process.env.ENDPOINT,
     database : process.env.DATABASE,
-    user     : process.env.USERNAME,
-    password : process.env.PASSWORD
+    user     : process.env.MYSQL_USER,
+    password : process.env.MYSQL_PASSWORD
   }
   );
 }
